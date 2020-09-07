29b280bb0b80f85f7e9b0802fb10904908001d52
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-07 21:12:48 +0300

    js-omg

--- projects from phpstorm/js/js.js
+++ projects from phpstorm/js/js.js
@@ -1,3 +1,67 @@
+let name, admin;
+name = "джон";
+admin = name;
+alert(admin);
+
+let planetName
+let yourName
+
+
+
+
+
+
+/*
+
+let name = "джон";
+
+
+let admin = "джон";
+console.log(admin);
+alert(admin);
+*/
+
+
+/*
 alert("hello");
 console.log("ПРИВЕТ");
-console.error("1");
+*/
+
+/*
+//alert("1+1");
+
+// переменная
+//  let name  = "Dima";
+//  console.log(name);
+
+//let name;
+//name = "dima";
+
+//let name = "dima",
+   // sername = "mao",
+   // age = 25
+*/
+
+
+/*
+let myName = yourName ="dima";
+console.log(myName)
+console.log(yourName)
+
+var age = 25;
+console.log(age);
+*/
+
+/*
+const myName ="vers"; //строго заданные
+console.log(myName);
+
+const COLOR_RED = "#f00"; // не вычесляемая константа
+
+
+function getMyStatus() {
+  return "success";
+}
+const myStatus =getMyStatus()
+
+*/
