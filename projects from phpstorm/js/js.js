8c51953f7fc319baf9cac08d1f2a5cb37eea4798
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-09 21:22:53 +0300

    l

--- projects from phpstorm/js/js.js
+++ projects from phpstorm/js/js.js
@@ -1,4 +1,39 @@
-"use strict";
+
+let a = 1, b = 1;
+
+let c = ++a; // ? 2
+let d = b++; // ? 1
+
+
+let a = 2;
+
+let x = 1 + (a *= 2);
+/*
+"" + 1 + 0 //  1 // "10"    -
+"" - 1 + 0 // 0 // -1       -
+true + false //  1 //       +
+6 / "3" //  Nan // 2        -
+"2" * "3" //    23// 6      -
+4 + 5 + "px"    // // 9px   -
+"$" + 4 + 5// $45           +
+"4" - 2 // 2                +
+"4px" - 2       //Nan       +
+7 / 0       // infiniti     +
+"  -9  " + 5 //" -9 5"      +
+"  -9  " - 5 // -14         -
+null + 1    // 1            +
+undefined + 1   // Nan      +
+" \t \n" - 2  // 0 -2 =-2   -
+*/
+
+
+
+/*
+let value = true;
+value = String(value);
+*/
+
+
 
 /*
 alert('Hello World');
@@ -54,11 +89,13 @@ let y =134;
 y = null;
 alert(y);
 */
+/*
 
 let st = "Дима";
 let st2 = "Привет";
 let sto = `кстати ${st + st2}`;
 alert(sto);
+*/
 
 
 /*
