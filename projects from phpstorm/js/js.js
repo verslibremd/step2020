05dc75d6ce0172317f66cad8c1b93df8be905641
verslibremd <68556094+verslibremd@users.noreply.github.com>
2020-09-17 21:54:05 +0300

    сделал обменник

--- projects from phpstorm/js/js.js
+++ projects from phpstorm/js/js.js
@@ -12,31 +12,90 @@ let fileSize = 820;
 alert((userVolume*CONSTANT_VOLUME) / fileSize);
 */
 
-/*
-Реализовать обмен валют.
-    Обменник должен уметь обменивать рубли на доллары и доллары на рубли.
-    Как должен работать обменник:
-    В константах необходимо хранить курсы обмена валют;
-Первым делом необходимо запросить у пользователя название валюты, которую он хочет обменять.
-    Далее пользователь должен иметь возможность ввести сумму, которую он хочет обменять;
-Далее обменник должен ввести пользователю информацию об операции.
-    А именно, показать сумму, которую получит клиент обратной валюте в случае обмена;
-Далее пользователь должен иметь возможность согласиться или отказаться от обмена.
-    Если пользователь соглашается с обменом, то выводим на экран сообщение
- “Мы поменяли {кол-во денег, которые передал клиент} {валюта, которую передал пользователь}
-на {кол-во денег, которые вернул обменник} {валюта, которую вернул обменник}.
-В противном случае выводим случае выводим сообщение “Увидимся в следующий раз”
-*/
 
-const RU_USA = 0.013;
-const USA_RU = 75.32;
-let name = prompt( "Валюта","RU или USA");
-if (name == "USA") {
+/*
+
+const RUB_USD = 0.013;
+const USD_RUB = 75.32;
+const RUB_EUR = 0.011;
+const EUR_RUB = 89.05;
+const USD_EUR = 0.85;
+const EUR_USD = 1.18;
+
+let name = prompt( "Валюта","RUB, USD, EUR");
+// USD
+if (name == "USD") {
     let sum = prompt("Сумма");
-    let final = sum*USA_RU;
-    alert(`Ваша сумма в рублях ${final}`);
-    let consent = confirm("Согласны ?");
-        if (consent == true) {
-            alert(`Мы поменяли ${sum + name } на ${final + "RU"}`);
+    let what = prompt("На какую валюту ?", "RUB, USD, EUR");
+    if (what == "RUB") {
+        let finalDollRub = sum*USD_RUB;
+        let consentFinalDollRub = confirm (`Ваша сумма в рублях ${finalDollRub} Продолжить обмен ?`);
+        if (consentFinalDollRub == true) {
+            alert(`Мы поменяли ${sum + "долларов" } на ${finalDollRub + "рублей"}`);
         }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
+    else if (what == "EUR") {
+        let finalDollEuro = sum*USD_EUR;
+        let consentFinalDollEuro = confirm (`Ваша сумма в евро ${finalDollEuro} Продолжить обмен ?`);
+        if (consentFinalDollEuro == true) {
+            alert(`Мы поменяли ${sum + "долларов" } на ${finalDollEuro + "евро"}`);
+        }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
 }
+// RUB
+else if (name == "RUB") {
+    let sum = prompt("Сумма");
+    let what = prompt("На какую валюту ?", "RUB, USD, EUR");
+    if (what == "USD") {
+        let finalRubDoll = sum*RUB_USD;
+        let consentFinalRubDoll = confirm (`Ваша сумма в долларах ${finalRubDoll} Продолжить обмен ?`);
+        if (consentFinalRubDoll == true) {
+            alert(`Мы поменяли ${sum + "рублей" } на ${finalRubDoll + "долларов"}`);
+        }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
+    else if (what == "EUR") {
+        let finalRubEuro = sum*RUB_EUR;
+        let consentFinalRubEuro = confirm (`Ваша сумма в евро ${finalRubEuro} Продолжить обмен ?`);
+        if (consentFinalRubEuro == true) {
+            alert(`Мы поменяли ${sum + "рублей" } на ${finalRubEuro + "евро"}`);
+        }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
+}
+// EUR
+else if (name == "EUR") {
+    let sum = prompt("Сумма");
+    let what = prompt("На какую валюту ?", "RUB, USD, EUR");
+    if (what == "USD") {
+        let finalEuroDoll = sum*EUR_USD;
+        let consentFinalEuroDoll = confirm (`Ваша сумма в долларах ${finalEuroDoll} Продолжить обмен ?`);
+        if (consentFinalEuroDoll == true) {
+            alert(`Мы поменяли ${sum + "евро" } на ${finalEuroDoll + "долларов"}`);
+        }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
+    else if (what == "RUB") {
+        let finalEuroRub = sum*EUR_RUB;
+        let consentFinalEuroRub = confirm (`Ваша сумма в рублях ${finalEuroRub} Продолжить обмен ?`);
+        if (consentFinalEuroRub == true) {
+            alert(`Мы поменяли ${sum + "евро" } на ${finalEuroRub + "рублей"}`);
+        }
+        else {
+            alert("Увидимся в следующий раз");
+        }
+    }
+}
+*/
