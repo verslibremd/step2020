ea3dadf0e8fc9967f7dc61dd59c1d7fb979500a2
Студент компьютерной Академии <step_student@IT-STEP.local>
2020-09-21 19:34:57 +0300

    sw

--- projects from phpstorm/js/js.js
+++ projects from phpstorm/js/js.js
@@ -1,4 +1,48 @@
 
+let month = prompt("введите номер месяца","1");
+switch (month) {
+    case "1" :
+        alert("январь");
+        break;
+    case "2" :
+        alert("февраль");
+            break;
+    case "3" :
+        alert("март");
+         break;
+    case "4" :
+        alert("апрель");
+           break;
+    case "5" :
+        alert("май");
+            break;
+    case "6" :
+        alert("июнь");
+           break;
+    case "7" :
+        alert("июль");
+         break;
+    case "8" :
+        alert("август");
+           break;
+    case "9" :
+        alert("сентябрь");
+              break;
+    case "10" :
+        alert("октябрь");
+            break;
+    case "11" :
+        alert("ноябрь");
+           break;
+    case "12" :
+        alert("декабрь");
+         break;
+    default:
+        alert("неверно введен месяц");
+}
+
+
+
 /*      1
 const THIS_YEAR = 2020;
 let yearOfBirth = prompt("Ваш год рождения");
