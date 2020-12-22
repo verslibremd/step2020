b5941e6c8f228c34d42721ead2fed2e86120202f
verslibremd <68556094+verslibremd@users.noreply.github.com>
2020-12-22 23:13:02 +0300

    dz4

--- PHP/lesson/dec21.php
+++ PHP/lesson/dec21.php
@@ -1,32 +1,33 @@
 <?php
-/*//1346
-
-//array_reverse
-$b = [];
+//1346
+//1
+$arr = [];
 for ($i=0;$i<10;$i++) {
-    $b[] = rand(10,100);
+    $arr[] = rand(10,100);
 }
+$arrRev = array_reverse($arr);
+?>
 
-$c = array_reverse($b);
-*/?><!--
 <p>
-<?php /*foreach ($b as $key => $value):*/?>
-<span><?/*=$value*/?></span>
-<?php /*endforeach*/?>
+<?php foreach ($arr as $key => $value):?>
+<span><?=$value?></span>
+<?php endforeach?>
 </p>
+
 <p>
-<?php /*foreach ($c as $key => $value):*/?>
-<span><?/*=$value*/?></span>
-<?php /*endforeach*/?>
-</p>-->
+<?php foreach ($arrRev as $key => $value):?>
+<span><?=$value?></span>
+<?php endforeach?>
+</p>
 
 <?php
-/*$path = $_SERVER['DOCUMENT_ROOT'] . '/img';
+//3
+$path = $_SERVER['DOCUMENT_ROOT'] . '/img';
 
 $bannersPath = [];
-$ij = scandir($path);
+$bann = scandir($path);
 
-foreach ($ij as $item) {
+foreach ($bann as $item) {
     if ($item !== '.' && $item !== '..') {
         $bannersPath[] = $item;
     }
@@ -35,28 +36,50 @@ var_dump($bannersPath);
 $randomIndex = rand(0, count($bannersPath) - 1);
 var_dump($randomIndex);
 $currentBanner = $bannersPath[$randomIndex];
-*/?>
+?>
 
-<!--<img src="img/<?/*=$currentBanner*/?>">-->
+<img src="img/<?=$currentBanner?>">
+
 
 <?php
+//4
 
-//array_fill_keys — Создает массив и заполняет его значениями с определенными ключами
-//range — Создает массив, содержащий диапазон элементов
+$a = array_fill(0,5, array());
 
-$zz = [];
-$ss = [];
-
-
-for ($i = 0; $i<5; $i++){
-    $zz[] = rand(10,100);
+for ($i = 0; $i < count($a); $i++){
+    for ($j = 0; $j<5; $j++){
+        $a[$i][$j] = rand(10,100);
+    }
 }
 
+//echo "<pre>";print_r($a);echo "</pre>";
+
+$aa = $a;
+for ($i = 0; $i < count($aa); $i++){
+    for ($j = 0; $j<count($i); $j++){
+        asort($aa[$i],SORT_NUMERIC);
+    }
+}
+//echo "<pre>";print_r($aa);echo "</pre>";
+
+?>
 
 
-$a = array_fill(0, 5, $zz);
+<?php foreach ($a as $key => $value):?>
+    <?php echo "<br>"?>
+    <?php foreach ($value as $item):?>
+        <span><?=$item?></span>
+    <?php endforeach?>
+<?php endforeach?>
+<hr>
+<?php foreach ($aa as $key => $value):?>
+    <?php echo "<br>"?>
+    <?php foreach ($value as $item):?>
+        <span><?=$item?></span>
+    <?php endforeach?>
+<?php endforeach?>
 
-echo "<pre>";print_r($a);echo "</pre>";
+
 
 
 
