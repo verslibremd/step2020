2747b27d07810d07b478d4dde5d615f7df46b9d1
verslibremd <68556094+verslibremd@users.noreply.github.com>
2021-01-12 22:36:20 +0300

    a

--- PHP/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-//https://oracleplsql.ru/mysql-manual.html
-//SELECT * FROM `users` WHERE gender = 2
-//SELECT * FROM `users` WHERE position_id = 1 OR position_id = 2
-//INSERT INTO `users` (`id`, `name`, `age`, `gender`, `position_id` ) VALUES (NULL, "Дима", "2", "1","4");
-
-//нужно выбрать всех сотрудников зарплата которых больше 50к
